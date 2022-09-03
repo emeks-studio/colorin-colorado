d45ae5a695a1b0c8d87cfad3628d89ffdacd51fb
mk-emeks <turco807@gmail.com>
2022-09-03 14:01:41 -0300

    Palette refactor + HexColor improvements and toJSON, fromJSON impls

--- src/ColorinColorado/Types/Palette.hs
+++ src/ColorinColorado/Types/Palette.hs
@@ -1,36 +1,55 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+
 module ColorinColorado.Types.Palette
-  ( Palette,
+  ( Palette, -- General purpose interface
     lookupHexColor,
     lookupByte,
-    buildPalette
+    SimplePalette256, -- Specific implementation
+    mkSimplePalette256, -- Smart constructor for SimplePalette256
   )
 where
 
-import Data.Word (Word8)
-import qualified Data.Bimap as Bimap (Bimap, lookup, lookupR, fromList)
-import ColorinColorado.Types.Colors (HexColorCode)
+import ColorinColorado.Types.Colors (HexColor)
+import Data.Aeson
+  ( FromJSON (parseJSON),
+    ToJSON (toJSON),
+    Value (Array),
+  )
+import qualified Data.Bimap as Bimap
+  ( Bimap,
+    fromList,
+    keysR,
+    lookup,
+    lookupR,
+  )
 import Data.List as List (length)
 import Data.List.Unique as List.Unique (allUnique)
+import Data.Word (Word8)
+import GHC.Generics (Generic)
+import qualified Data.Foldable (toList)
 
 class Palette a where
-    lookupHexColor :: a -> Word8 -> Maybe HexColorCode
-    lookupByte :: a -> HexColorCode -> Maybe Word8
+  lookupHexColor :: a -> Word8 -> Maybe HexColor
+  lookupByte :: a -> HexColor -> Maybe Word8
 
--- TODO: Implement toJSON AND fromJSON
-newtype MyPalette = MyPalette (Bimap.Bimap Word8 HexColorCode)
+newtype SimplePalette256 = SimplePalette256 (Bimap.Bimap Word8 HexColor)
+  deriving stock (Generic, Eq, Ord, Show)
 
-instance Palette MyPalette where
-    lookupHexColor (MyPalette palette) byte = Bimap.lookup byte palette
-    lookupByte (MyPalette palette) color = Bimap.lookupR color palette
+instance Palette SimplePalette256 where
+  lookupHexColor (SimplePalette256 palette) byte = Bimap.lookup byte palette
+  lookupByte (SimplePalette256 palette) color = Bimap.lookupR color palette
 
-buildPalette :: [Word8] -> [HexColorCode] -> Either String MyPalette
-buildPalette bytes colors = do
+mkSimplePalette256 :: [HexColor] -> Either String SimplePalette256
+mkSimplePalette256 colors = do
   _ <- has256Elements colors "Palette must have 256 colors"
-  _ <- has256Elements bytes "Palette must have 256 bytes"
   _ <- notRepeatedElements colors "Palette must have all unique colors"
-  _ <- notRepeatedElements bytes "Palette must have all unique bytes"
-  let tupleList = zip bytes colors
-  return $ MyPalette $ Bimap.fromList tupleList
+  let tupleList = zip possibleBytes colors
+  return $ SimplePalette256 $ Bimap.fromList tupleList
+
+possibleBytes :: [Word8]
+possibleBytes = [0 .. 255]
 
 has256Elements :: [a] -> String -> Either String ()
 has256Elements list errorMessage =
@@ -40,9 +59,18 @@ notRepeatedElements :: Ord a => [a] -> String -> Either String ()
 notRepeatedElements list errorMessage =
   if List.Unique.allUnique list then Right () else Left errorMessage
 
--- TODO: buildPalette with shuffle possible bytes!
+-- JSON representation is just the list of HexColor
+instance ToJSON SimplePalette256 where
+  toJSON (SimplePalette256 bimap) = toJSON $ Bimap.keysR bimap
+
+instance FromJSON SimplePalette256 where
+  parseJSON (Array xs) = do
+    colors <- mapM parseJSON $ Data.Foldable.toList xs
+    case mkSimplePalette256 colors of
+      Left errorMessage -> fail $ "Decode SimplePalette256: " <> errorMessage
+      Right palette -> return palette
+  parseJSON _ = fail "Decode SimplePalette256: Expected an array of HexColors"
+
+-- TODO: mkSimplePalette256 with shuffle colors!
 -- Use shuffleM :: MonadRandom m => [a] -> m [a]
 -- ^ lib: random-shuffle
-
-allBytes :: [Word8]
-allBytes = [0 .. 255]
