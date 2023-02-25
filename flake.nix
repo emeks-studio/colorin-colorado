eaaab22db9c9de0666e0ad4360d7cc998bf97a38
mk-emeks <turco807@gmail.com>
2023-02-25 14:25:57 -0300

    update to ghc925 + hls + hlint + ormolu + pre-commit

--- flake.nix
+++ flake.nix
@@ -1,9 +1,31 @@
 {
-  # This is a template created by `hix init` (nix flake init --template templates#haskell-nix)
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  # Initially this is a template created by `hix init` (nix flake init --template templates#haskell-nix)
+  # --- Flake Local Nix Configuration ----------------------------
+  nixConfig = {
+    # This sets the flake to use the IOG nix cache (and others).
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = [
+      "https://cache.iog.io" 
+      "https://pre-commit-hooks.cachix.org"
+      "https://emeks-public.cachix.org" 
+       # ^ Use https://docs.cachix.org/pushing#pushing-flake-inputs to update this!
+       # TODO: Push to the cache in order to save time!
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" 
+      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc=" 
+      "emeks-public.cachix.org-1:sz2oZuYq7EsRb5FW6sDtpPU1CWh+6ymOgxFgmrYTKGI="
+    ];
+    allow-import-from-derivation = "true";
+    bash-prompt = "\\[\\e[0;37m\\](\\[\\e[0m\\]nix) \\[\\e[0;1;91m\\]colorin-colorado \\[\\e[0m\\]\\w \\[\\e[0;1m\\]λ \\[\\e[0m\\]";
+  };
+
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix/986ebab4075d4c1ecac1270a9304e65e7ef715bc";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+  outputs = { self, nixpkgs, pre-commit-hooks, flake-utils, haskellNix }:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -21,6 +43,14 @@
               final.haskell-nix.hix.project {
                 src = ./.;
                 evalSystem = system;
+                compiler-nix-name = "ghc925";
+                shell.tools = {
+                  cabal = {};
+                  hlint = {};
+                  ormolu = {};
+                  # https://haskell-language-server.readthedocs.io/en/latest/support/ghc-version-support.html
+                  haskell-language-server = "1.9.1.0";
+                };
               };
           })
         ];
@@ -30,15 +60,24 @@
         legacyPackages = pkgs;
 
         packages.default = flake.packages."colorin-colorado:exe:colorin-colorado";
-      });
 
-  # --- Flake Local Nix Configuration ----------------------------
-  nixConfig = {
-    # This sets the flake to use the IOG nix cache.
-    # Nix should ask for permission before using it,
-    # but remove it here if you do not want it to.
-    extra-substituters = ["https://cache.iog.io" "https://emeks-public.cachix.org"];
-    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "emeks-public.cachix.org-1:sz2oZuYq7EsRb5FW6sDtpPU1CWh+6ymOgxFgmrYTKGI="];
-    allow-import-from-derivation = "true";
-  };
+        # Execute with "nix flake check"
+        # ^ FIXME: Properly install pre-commit after enter in "nix develop"
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              ormolu.enable = true;
+            };
+        };
+
+        devShell = pkgs.mkShell {
+          shellHook = ''
+            echo "Installing pre-commit hooks";
+            ${self.checks.pre-commit-check.shellHook}
+          '';
+        };
+      };
+    });  
 }
