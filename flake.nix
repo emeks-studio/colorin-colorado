ac7971729e7f71ce947430379deb388e8ad6aca7
mk-emeks <turco807@gmail.com>
2023-02-25 14:16:01 -0300

    simplify nix flakes

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  # This is a template created by `hix init`
+  # This is a template created by `hix init` (nix flake init --template templates#haskell-nix)
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
@@ -7,9 +7,10 @@
     let
       supportedSystems = [
         "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
+        # Not for now!
+        # "x86_64-darwin"
+        # "aarch64-linux"
+        # "aarch64-darwin"
       ];
     in
       flake-utils.lib.eachSystem supportedSystems (system:
@@ -19,7 +20,7 @@
             hixProject =
               final.haskell-nix.hix.project {
                 src = ./.;
-                evalSystem = "x86_64-linux";
+                evalSystem = system;
               };
           })
         ];
@@ -27,6 +28,8 @@
         flake = pkgs.hixProject.flake {};
       in flake // {
         legacyPackages = pkgs;
+
+        packages.default = flake.packages."colorin-colorado:exe:colorin-colorado";
       });
 
   # --- Flake Local Nix Configuration ----------------------------
@@ -34,8 +37,8 @@
     # This sets the flake to use the IOG nix cache.
     # Nix should ask for permission before using it,
     # but remove it here if you do not want it to.
-    extra-substituters = ["https://cache.iog.io"];
-    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    extra-substituters = ["https://cache.iog.io" "https://emeks-public.cachix.org"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "emeks-public.cachix.org-1:sz2oZuYq7EsRb5FW6sDtpPU1CWh+6ymOgxFgmrYTKGI="];
     allow-import-from-derivation = "true";
   };
 }
