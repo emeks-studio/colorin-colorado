79b9c532dd8c1c77e151004d787dc549b0085fd8
mk <mirko.alic@plowtech.net>
2023-08-05 16:25:43 -0300

    cleanup

--- gen-example/Main.hs
+++ gen-example/Main.hs
@@ -1,8 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-missing-import-lists #-}
-{-# OPTIONS_GHC -Wno-unused-local-binds #-}
 
--- | More docs at https://github.com/diagrams/svg-builder
+-- | Reference:
+-- * Arc (absolute)
+--    aA :: RealFloat a =>  a -> a -> a -> a -> a -> a -> a -> Text
+--    aA rx ry xrot largeFlag sweepFlag x y = T.concat
+--      [ "A ", toText rx, ",", toText ry, " ", toText xrot, " ", toText largeFlag
+--      , " ", toText sweepFlag, " ", toText x, " ", toText y, " "]
+-- * Arc (relative)
+--    aR :: RealFloat a =>  a -> a -> a -> a -> a -> a -> a -> Text
+--    aR rx ry xrot largeFlag sweepFlag x y = _
+-- ^ More docs at https://github.com/diagrams/svg-builder
 module Main where
 
 import qualified Data.Foldable as F
@@ -12,10 +20,37 @@ import qualified Data.Text as T
 import Graphics.Svg
 import System.Environment (getArgs)
 
-svg :: Element -> Element
-svg content =
-  doctype
-    <> with (svg11_ content) [Version_ <<- "1.1", Width_ <<- "300", Height_ <<- "300"]
+-- | Example: cabal v2-run gen-example -- example.svg
+main :: IO ()
+main = do
+  [filename] <- getArgs
+  let filenameInCurrentDirectoryPath :: FilePath
+      filenameInCurrentDirectoryPath = "./" ++ filename
+      -- Black circle in the center (img of 300x300)
+      baseShape :: Element
+      baseShape =
+        circle_
+          [ Cx_ <<- "150",
+            Cy_ <<- "150",
+            R_ <<- T.pack (show radius),
+            Stroke_ <<- "black",
+            Fill_ <<- "black"
+          ]
+      -- From a base black circle iterate slices over the regions
+      contents :: Element
+      contents =
+        F.foldr
+          (\iterator accShape -> accShape <> slice iterator)
+          baseShape
+          [0 .. (fromIntegral $ M.size regions - 1)]
+      svgCanvas :: Element -> Element
+      svgCanvas content =
+        doctype
+          <> with (svg11_ content) [Version_ <<- "1.1", Width_ <<- "300", Height_ <<- "300"]
+      svgBytes :: String
+      svgBytes = show $ svgCanvas contents
+  -- Obs: Completly overrides the existing file
+  writeFile filenameInCurrentDirectoryPath svgBytes
 
 zero :: Float
 zero = 0
@@ -33,14 +68,10 @@ centerY = 150
 radius :: Float
 radius = 10
 
---degreesIterator :: Float
---degreesIterator = pi / 6 -- 30. degrees
-
 -- TODO: Use to iterate more than 1 ring (you can start from 0)
 ringLevel :: Float
 ringLevel = 1
 
--- | FIXME: For the bottom half of the circle, we need to swap things! (for now is a rainbow)
 regions :: M.Map Int T.Text
 regions =
   M.fromList
@@ -49,7 +80,7 @@ regions =
       (2, "yellow"),
       (3, "cyan"),
       (4, "purple"),
-      (5, "indigo"), -- til here goes ok
+      (5, "indigo"),
       (6, "violet"),
       (7, "magenta"),
       (8, "orange"),
@@ -58,7 +89,7 @@ regions =
       (11, "grey")
     ]
 
--- | 360 degress/ 30 degrees = 12 regions
+-- | 360 degres (2 * pi) / 30 degrees (pi / 6) = 12 regions
 angleByRegion :: Int -> Float
 angleByRegion 0 = pi / 6
 angleByRegion 1 = pi / 3
@@ -74,34 +105,35 @@ angleByRegion 10 = 11 * pi / 6
 angleByRegion 11 = pi / 6
 angleByRegion _ = 0
 
-arcByRegion :: Int -> Int
-arcByRegion n
+largeArcFlagByRegion :: Int -> Int
+largeArcFlagByRegion n
   | n <= 5 = 0
-  | otherwise = 1
+  | otherwise = 1 -- large arc
 
-sweepSmall :: Int -> Int
-sweepSmall n
-  | n == 11 = 1
+outerSweepFlagByRegion :: Int -> Int
+outerSweepFlagByRegion n
+  | n == 11 = 1 -- clockwise
   | otherwise = 0
 
-sweepLarge :: Int -> Int
-sweepLarge n
+innerSweepFlagByRegion :: Int -> Int
+innerSweepFlagByRegion n
   | n == 11 = 0
-  | otherwise = 1
+  | otherwise = 1 -- clockwise
 
--- we should iterate over degress!
+xAxisRotation :: Float
+xAxisRotation = zero
+
 slice :: Int -> Element
 slice iterator =
-  ( path_
-      [ D_ <<- mA p1x p2y -- center
-          <> lA p2x p2y
-          <> aA rA1 rA1 zero (arcByRegion iterator) (sweepSmall iterator) p3x p3y
-          <> lA p4x p4y
-          <> aA rA2 rA2 zero (arcByRegion iterator) (sweepLarge iterator) p1x p1y,
-        Stroke_ <<- "white",
-        Fill_ <<- regions ! (iterator)
-      ]
-  )
+  path_
+    [ D_ <<- mA p1x p1y -- initial point
+        <> lA p2x p2y
+        <> aA rA1 rA1 xAxisRotation (largeArcFlagByRegion iterator) (outerSweepFlagByRegion iterator) p3x p3y
+        <> lA p4x p4y
+        <> aA rA2 rA2 xAxisRotation (largeArcFlagByRegion iterator) (innerSweepFlagByRegion iterator) p1x p1y,
+      Stroke_ <<- "white",
+      Fill_ <<- regions ! iterator
+    ]
   where
     p1x = 150 + ringLevel * radius
     p1y = 150
@@ -113,30 +145,3 @@ slice iterator =
     p4x = 150 + ringLevel * radius * cos (angleByRegion iterator)
     p4y = 150 - ringLevel * radius * sin (angleByRegion iterator)
     rA2 = ringLevel * radius
-
-baseShape :: Element
-baseShape = circle_ [Cx_ <<- "150", Cy_ <<- "150", R_ <<- (T.pack $ show radius), Stroke_ <<- "black", Fill_ <<- "black"]
-
-contents :: Element
-contents = F.foldr (\iterator accShape -> accShape <> slice iterator) baseShape [0 .. (fromIntegral $ M.size regions - 1)]
-
--- -- | Arc (absolute)
---aA :: RealFloat a =>  a -> a -> a -> a -> a -> a -> a -> Text
---aA rx ry xrot largeFlag sweepFlag x y = T.concat
---  [ "A ", toText rx, ",", toText ry, " ", toText xrot, " ", toText largeFlag
---  , " ", toText sweepFlag, " ", toText x, " ", toText y, " "]
-
--- | Arc (relative)
--- aR :: RealFloat a =>  a -> a -> a -> a -> a -> a -> a -> Text
--- aR rx ry xrot largeFlag sweepFlag x y =
-
--- | Example: cabal v2-run gen-example -- example.svg
-main :: IO ()
-main = do
-  [filename] <- getArgs
-  let filenameInCurrentDirectoryPath :: FilePath
-      filenameInCurrentDirectoryPath = "./" ++ filename
-      svgContent :: String
-      svgContent = show $ svg contents
-  -- Obs: Completly overrides the existing file
-  writeFile filenameInCurrentDirectoryPath svgContent
